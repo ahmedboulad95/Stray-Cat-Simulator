81df62c15481096df8bd397cca3d870cee87a76f
Ahmed Boulad <ahmedboulad95@gmail.com>
2020-04-16 20:12:42 -0500

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100.0f;
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private CharacterController controller;
+
+    [SerializeField] private float walkSpeed = 12.0f;
+    [SerializeField] private float runSpeed = 24.0f;
+    [SerializeField] private float gravity = 5.0f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float speed = walkSpeed;
+
+        if (Input.GetKey("left shift"))
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isWalking", false);
+            speed = runSpeed;
+        }
+        else if (x != 0f || z != 0f)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+
+        Vector3 move = transform.right * x + -transform.up * gravity + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
